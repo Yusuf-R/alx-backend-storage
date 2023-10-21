d54f62856dd5df68c5fed5e5d77dff333f16f3c8
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-22 00:36:59 +0100

    fix: :bug: increase the match case for the last meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -10,4 +10,4 @@ CREATE VIEW need_meeting
 AS SELECT * 
 FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < NOW() - INTERVAL 1 MONTH);
+AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
