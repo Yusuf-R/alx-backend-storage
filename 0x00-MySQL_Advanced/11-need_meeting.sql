d7eaa4a3b4886d7057ca35b490407239c3b2eddd
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-22 00:42:38 +0100

    fix: :bug: fix the logic inconsistencies

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -6,8 +6,12 @@
 -- They score are under (strict) to 80
 -- AND no last_meeting date OR more than a month
 
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting
-AS SELECT * 
+AS SELECT name 
 FROM students
 WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+AND (
+  last_meeting IS NULL 
+  OR last_meeting < ADDDATE(CURRDATE(), INTERVAL -1 MONTH)
+  );
