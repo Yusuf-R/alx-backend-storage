8dd2c21df9efa84480c502e32a064c6a714b4da7
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-22 16:30:12 +0100

    feat: :sparkles: fucntion returning the list of documents in a collecton

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-""" List documents """
-import pymongo
+"""
+List all documents in a MongoDB collection.
+
+Args:
+    mongo_collection: A MongoDB collection object.
+
+Returns:
+    A list of documents in the collection.
+    If the collection is empty, an empty list is returned.
+
+Raises:
+    None.
+"""
 
 
 def list_all(mongo_collection) -> list:
-    """ Lists all documents in a collection
-        Args:
-            mongo_collection: Collection of object
-
-        Return:
-            List with documents, otherwise []
-    """
-    documents: list = []
-
-    for document in mongo_collection.find():
-        documents.append(document)
-
-    return documents
+    if mongo_collection.estimated_document_count() == 0:
+        return []
+    return list(mongo_collection.find())
