e256f149117de8dbb9d0e36e36273ed63615e202
Yusuf-R <y.abdulwasiu@gmail.com>
2023-10-22 16:42:08 +0100

    fix: :bug: doc pycodestyle comp

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python3
-import pymongo
-"""
-List all documents in a MongoDB collection.
-
-Args:
-    mongo_collection: A MongoDB collection object.
-
-Returns:
-    A list of documents in the collection.
-    If the collection is empty, an empty list is returned.
-
-Raises:
-    None.
-"""
+""" List of documents """
 
 
 def list_all(mongo_collection) -> list:
-    """ List all documents in a MongoDB collection."""
+    """
+    List all documents in a MongoDB collection.
+    Args:
+        mongo_collection: A MongoDB collection object.
+    Returns:
+        A list of documents in the collection.
+        If the collection is empty, an empty list is returned.
+    Raises:
+        None.
+    """
     doc_list: list = []
     for doc in mongo_collection.find():
         doc_list.append(doc)
