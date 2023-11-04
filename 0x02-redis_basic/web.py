a57a67bd3cc4e0cc3c1eab1a2b584835f8358d33
Yusuf-R <y.abdulwasiu@gmail.com>
2023-11-05 00:58:36 +0100

    feat: :sparkles: same old, but uses f-string

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -53,21 +53,21 @@ def cache_and_count(func: Callable) -> Callable:
         """
 
         # create cache and count keys
-        cache_key: str = "cache:{}".format(url)
-        count_key: str = "count:{}".format(url)
+        # cache_key: str = "cache:{}".format(url)
+        # count_key: str = "count:{}".format(url)
 
         # icreament the count first, kinda crazy though
-        redis_client.incr(count_key)
+        redis_client.incr(f'count:{url}')
 
         # check if cached content exists
-        cached_content = redis_client.get(cache_key)
+        cached_content = redis_client.get(f'cache:{url}')
         if cached_content:
             return cached_content.decode("utf-8")
 
         # get content from url
         html = func(url)
         # cache the content with expiration of 10secs
-        redis_client.setex(cache_key, 10, html)
+        redis_client.setex(f'cache:{url}', 10, html)
         return html
 
     return wrapper
